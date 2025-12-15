3396f6d662bce8a08685a6de6a263259de18968d
sunthing <apple0908070605@gmail.com>
2025-12-15 16:18:26 +0800

    更改標題與副標

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,4 +1,4 @@
   <h1 class="site-heading text-center text-white d-none d-lg-block">
-    <span class="site-heading-upper text-primary mb-3">A Free Bootstrap 4 Business Theme</span>
-    <span class="site-heading-lower">Business Casual</span>
+    <span class="site-heading-upper text-primary mb-3">千年傳統，全新感受</span>
+    <span class="site-heading-lower">鄒族阿里山咖啡</span>
   </h1>
